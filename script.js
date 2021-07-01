7f83ac8699c396ca3a3c7a1fde151b14ac2aa46d
Nicolas Goosen <nicolasgoosen@icloud.com>
2021-07-01 15:51:30 +0200

    inputs linked up

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 
 
-let output = getElementById('output');
+let output = document.getElementById('output');
+
+document.querySelectorAll('input').forEach(input => {
+    input.addEventListener('input', inputUpdated);
+});
+
+function inputUpdated(e) {
+    document.documentElement.style.setProperty(
+        `--${e.target.name}`,
+        e.target.value
+    );
+}
