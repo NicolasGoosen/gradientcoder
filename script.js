a13e46374953be56180bfe44eded3fe73b985106
Nicolas Goosen <nicolasgoosen@icloud.com>
2021-07-01 16:05:44 +0200

    textbox edits heading

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-
-
 let output = document.getElementById('output');
+let showtext = document.querySelector('h2');
 
-document.querySelectorAll('input').forEach(input => {
+document.querySelectorAll('input:not([type="text"])').forEach(input => {
     input.addEventListener('input', inputUpdated);
 });
+
+document.querySelector('input[type="text"]').addEventListener('input', textUpdated);
 
 function inputUpdated(e) {
     document.documentElement.style.setProperty(
         `--${e.target.name}`,
         e.target.value
     );
+}
+
+function textUpdated(e) {
+    console.log(e.target.value);
+    showtext.innerHTML = e.target.value == '' ? 'Type Something' : e.target.value;
 }
